4f0abff8f64bb1c8ba766dbff1760e022612015b
jonaylor89 <jonaylor89@gmail.com>
2017-09-01 13:06:33 -0400

    Addition of header

--- Hello.java
+++ Hello.java
@@ -1,5 +1,20 @@
+/*
+ * John Naylor
+ * CMSC 255 
+ * Lab 1, Hello World
+ */
+
 public class Hello{
   public static void main(String[] argv){
+    printHeader();
     System.out.println("Hello World");
   }
+  
+  public static void printHeader(){
+    System.out.println("John Naylor");
+    System.out.println("CMSC255, Section 2");
+    System.out.println("Hello World");
+    System.out.println("Lab 1");
+  }
+  
 }
