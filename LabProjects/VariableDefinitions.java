8333b54d1f128e0d20cb0eb2fe9f2d9bbadcc3d1
jonaylor89 <jonaylor89@gmail.com>
2017-09-14 16:34:03 -0400

    Restructuring Directory

---
+++ LabProjects/VariableDefinitions.java
@@ -0,0 +1,102 @@
+
+public class VariableDefinitions{
+
+  public static void main(String[] argv){
+
+    System.out.println("John Naylor, Banchau Tran, Session 002");
+
+    final int INT = 42;
+    final boolean BOOL = true;
+    final byte BYTE = 12;
+    final float FLOAT = 11.0F;
+    final char CHAR = 'j';
+    final double DOUBLE = 3.141592;
+    final short SHORT = 50;
+    final long LONG = 314159265;
+
+
+    byte numPeople = 3;
+    float itemPrice = 32.32F;
+    boolean flag = false;
+    char dataValue = 'a';
+    int roomNum = 207;
+    float dinnerPrice = 30.0F;
+    long bigNum = 990123456789L;
+    int longishNum = 177609;
+
+    int count = 42;
+    double total = 42.0;
+    char stopKey = 'a';
+    boolean stopFlag = false;
+    float temperatureReading = 42.0F;
+    byte numPets = 42;
+    short littleNum = 9;
+    long reallyBigNumber = 31745832;
+
+    System.out.println("The numPeople variable has a value of " + numPeople);
+    System.out.println("The itemPrice variable has a value of " + itemPrice);
+    System.out.println("The flag variable has a value of " + flag);
+    System.out.println("The dataValue variable has a value of " + dataValue);
+    System.out.println("The roomNum variable has a value of " + roomNum);
+    System.out.println("The dinnerPrice variable has a value of " + dinnerPrice);
+    System.out.println("The bigNum variable has a value of " + bigNum);
+    System.out.println("The longishNum variable has a value of " + longishNum);
+    System.out.println("The count variable has a value of " + count);
+    System.out.println("The total variable has a value of " + total);
+    System.out.println("The stopKey variable has a value of " + stopKey);
+    System.out.println("The stopFlag variable has a value of " + stopFlag);
+    System.out.println("The temperatureReading variable has a value of " + temperatureReading);
+    System.out.println("The numPets variable has a value of " + numPets);
+    System.out.println("The littleNum variable has a value of " + littleNum);
+    System.out.println("The reallyBigNumber variable has a value of " + reallyBigNumber);
+    System.out.println();
+
+    numPeople = 6;
+    itemPrice = 36.32F;
+    flag = true;
+    dataValue = 'b';
+    roomNum = 200;
+    dinnerPrice = 42.0F;
+    bigNum = 23456789;
+    longishNum = 177611;
+
+    count = 12;
+    total = 50.0;
+    stopKey = 'z';
+    stopFlag = true;
+    temperatureReading = 60.0F;
+    numPets = 13;
+    littleNum = 3;
+    reallyBigNumber = 543210;
+
+    System.out.println("The numPeople variable has a value of " + numPeople);
+    System.out.println("The itemPrice variable has a value of " + itemPrice);
+    System.out.println("The flag variable has a value of " + flag);
+    System.out.println("The dataValue variable has a value of " + dataValue);
+    System.out.println("The roomNum variable has a value of " + roomNum);
+    System.out.println("The dinnerPrice variable has a value of " + dinnerPrice);
+    System.out.println("The bigNum variable has a value of " + bigNum);
+    System.out.println("The longishNum variable has a value of " + longishNum);
+    System.out.println("The count variable has a value of " + count);
+    System.out.println("The total variable has a value of " + total);
+    System.out.println("The stopKey variable has a value of " + stopKey);
+    System.out.println("The stopFlag variable has a value of " + stopFlag);
+    System.out.println("The temperatureReading variable has a value of " + temperatureReading);
+    System.out.println("The numPets variable has a value of " + numPets);
+    System.out.println("The littleNum variable has a value of " + littleNum);
+    System.out.println("The reallyBigNumber variable has a value of " + reallyBigNumber);
+    System.out.println();
+
+    System.out.println("The int constant has a value of " + INT);
+    System.out.println("The boolean constant has a value of " + BOOL);
+    System.out.println("The byte constant has a value of " + BYTE);
+    System.out.println("The float constant has a value of " + FLOAT);
+    System.out.println("The character constant has a value of " + CHAR);
+    System.out.println("The double constant has a value of " + DOUBLE);
+    System.out.println("The short constant has a value of " + SHORT);
+    System.out.println("The long constant has a value of " + LONG);
+
+    //INT = 5;
+
+  }
+}
---
+++ ProgrammingProjects/RocketCalculator.java
@@ -0,0 +1,113 @@
+
+/**
+ * John Naylor
+ * CMSC 255, 2
+ * Programming 2, Rocket Calculator
+ *
+ * This program calculates
+ * the cost of building a Rocket
+ * including Fuel Cost
+ *
+ * The user must enter the dimensions
+ * of the rocket and the price of the fuel
+ *
+ *
+ *
+ * Pseudocode:
+ * [+] Print header
+ * [+] User inputs the cylinder height, cylinder radius, cone height, fin height, and price of fuel
+ * [+] Calculation of cylinder surface area
+ * [+] Calculation of cone surface area
+ * [+] Calculation of the fine surface area
+ * [+] Total surface area i calculated by the three previous calcutions added together
+ * [+] Calcution of the cost to build the rocket
+ * [+] Calcution of the amount of fuel
+ * [+] Calcution of the total cost for fuel
+ * [+] Output the results of the previous calculations
+ * [+] If the user entered the wrong data type anywhere and the program had a error print NaN and restart the program
+ *
+ */
+
+
+ import java.util.*;
+ import java.lang.Math;
+
+ public class RocketCalculator{
+
+   public static void main(String[] argv){
+
+     Scanner userInput = new Scanner(System.in);
+
+     System.out.println("John Naylor");
+     System.out.println("CMSC255, Section 2");
+     System.out.println("Rocket Calculator");
+     System.out.println("Programming project 2");
+
+     int HeightCylinder;
+     int RadiusCylinder;
+     int HeightCone;
+     int HeightFin;
+     int BaseFin;
+     double PriceFuel;
+
+     try{
+       System.out.print("Enter the height of the cylinder of the rocket(cm)  ");
+       HeightCylinder = userInput.nextInt();
+
+       System.out.print("Enter the radius of the cylinder of the rocket(cm)  ");
+       RadiusCylinder = userInput.nextInt();
+
+       System.out.print("Enter the height of the cone of the rocket(cm)  ");
+       HeightCone = userInput.nextInt();
+
+       System.out.print("Enter the height of the fin of the rocket(cm)  ");
+       HeightFin = userInput.nextInt();
+
+       System.out.print("Enter the base of the fin of the rocket(cm)  ");
+       BaseFin = userInput.nextInt();
+
+       System.out.print("Enter the price of the fuel for the rocket($/L)  ");
+       PriceFuel = userInput.nextDouble();
+
+
+       double CylinderSurfArea = surfAreaOfCylinder(RadiusCylinder, HeightCylinder);
+       double ConeSurfArea = surfAreaOfCone(RadiusCylinder, HeightCone);
+       double FinSurfArea = areaOfParallel(BaseFin, HeightFin);
+       double TotalSurfArea = CylinderSurfArea + ConeSurfArea + FinSurfArea;
+       double CostToBuild = TotalSurfArea * 1.75;
+       double AmountOfFuel = volumeOfCylinder(RadiusCylinder, HeightCylinder);
+       double CostOfFuel = AmountOfFuel * PriceFuel;
+
+       System.out.println("The rocket measurements are:");
+       System.out.printf("\t Cylinder Surface Area: %.2f(cm^2)\n", CylinderSurfArea);
+       System.out.printf("\t Cone Surface Area: %.2f(cm^2)\n", ConeSurfArea);
+       System.out.printf("\t Fin Surface Area: %.2f x 3 = %.2f(cm^2)\n", FinSurfArea, FinSurfArea * 3);
+       System.out.printf("\t Total Surface Area: %.2f(cm^2)\n", TotalSurfArea);
+
+       System.out.printf("Cost of the Material: $%.2f\n", CostToBuild);
+       System.out.printf("The Rocket Uses: %.2f(L)\n", AmountOfFuel);
+       System.out.printf("The Total Cost of Fuel: $%.2f\n", CostOfFuel);
+
+     }catch(InputMismatchException e){
+       System.out.println("Not appropriate datatype, restart program");
+     }
+
+   }
+
+   public static double volumeOfCylinder(int radius, int height){
+     return Math.PI * radius * radius * height;
+   }
+
+   public static double surfAreaOfCylinder(int radius, int height){
+     return 2 * Math.PI * radius * height + 2 * Math.PI * radius * radius;
+   }
+
+   public static double surfAreaOfCone(int height, int radius){
+     return Math.PI * radius * Math.sqrt((double)(height * height + radius * radius));
+   }
+
+   public static int areaOfParallel(int base, int height){
+     return base * height;
+   }
+
+ }
