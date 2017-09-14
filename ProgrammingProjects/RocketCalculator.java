35424cd1ebd8ac201bf18991493bb4d2c77b2810
jonaylor89 <jonaylor89@gmail.com>
2017-09-14 16:51:10 -0400

    Adding Documentation

--- ProgrammingProjects/RocketCalculator.java
+++ ProgrammingProjects/RocketCalculator.java
@@ -36,13 +36,16 @@
 
    public static void main(String[] argv){
 
+     // Create a new scanner that will be used to scan from user input
      Scanner userInput = new Scanner(System.in);
 
+     //Header Documentation
      System.out.println("John Naylor");
      System.out.println("CMSC255, Section 2");
      System.out.println("Rocket Calculator");
      System.out.println("Programming project 2");
 
+     //Declare the needed variables
      int HeightCylinder;
      int RadiusCylinder;
      int HeightCone;
@@ -51,6 +54,8 @@
      double PriceFuel;
 
      try{
+
+       // Retreive all of the needed parameters from the user.
        System.out.print("Enter the height of the cylinder of the rocket(cm)  ");
        HeightCylinder = userInput.nextInt();
 
@@ -70,6 +75,7 @@
        PriceFuel = userInput.nextDouble();
 
 
+       //Keeping all of the output variables as doubles for maximum percision.
        double CylinderSurfArea = surfAreaOfCylinder(RadiusCylinder, HeightCylinder);
        double ConeSurfArea = surfAreaOfCone(RadiusCylinder, HeightCone);
        double FinSurfArea = areaOfParallel(BaseFin, HeightFin);
@@ -78,6 +84,8 @@
        double AmountOfFuel = volumeOfCylinder(RadiusCylinder, HeightCylinder);
        double CostOfFuel = AmountOfFuel * PriceFuel;
 
+
+       // Print all of the output to the console with the appropriate decimal percision
        System.out.println("The rocket measurements are:");
        System.out.printf("\t Cylinder Surface Area: %.2f(cm^2)\n", CylinderSurfArea);
        System.out.printf("\t Cone Surface Area: %.2f(cm^2)\n", ConeSurfArea);
@@ -88,11 +96,20 @@
        System.out.printf("The Rocket Uses: %.2f(L)\n", AmountOfFuel);
        System.out.printf("The Total Cost of Fuel: $%.2f\n", CostOfFuel);
 
+       // If the user puts in an incompatable data type, the program tells the user and quits.
      }catch(InputMismatchException e){
        System.out.println("Not appropriate datatype, restart program");
      }
 
    }
+
+   /*
+
+     The following are the collection
+     of math formulas used to calculate
+     the output.
+
+   */
 
    public static double volumeOfCylinder(int radius, int height){
      return Math.PI * radius * radius * height;
