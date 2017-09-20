48a9d49c54a1577bbe07f512d049efb8aa335979
jonaylor89 <jonaylor89@gmail.com>
2017-09-20 09:28:12 -0400

    At this point I'm just being fancy

--- LabProjects/PhoneNumbers.java
+++ LabProjects/PhoneNumbers.java
@@ -24,33 +24,35 @@ public class PhoneNumbers{
 
     System.out.println("User enters an " + digit);
 
-    if((digit == 'A') || (digit == 'B') || (digit == 'C')){
-      number = 2;
-    }
-    else if((digit == 'D') || (digit == 'E') || (digit == 'F')){
-      number = 3;
-    }
-    else if((digit == 'G') || (digit == 'H') || (digit == 'I')){
-      number = 4;
-    }
-    else if((digit == 'J') || (digit == 'K') || (digit == 'L')){
-      number = 5;
-    }
-    else if((digit == 'M') || (digit == 'N') || (digit == 'O')){
-      number = 6;
-    }
-    else if((digit == 'P') || (digit == 'Q') || (digit == 'R') || (digit == 'S')){
-      number = 7;
-    }
-    else if((digit == 'T') || (digit == 'U') || (digit == 'V') || (digit == 'W')){
-      number = 8;
-    }
-    else if((digit == 'X') || (digit == 'Y') || (digit == 'Z')){
-      number = 9;
-    }
-    else{
-      System.out.println("Please enter a valid number");
-    }
+    do{
+      if((digit == 'A') || (digit == 'B') || (digit == 'C')){
+        number = 2;
+      }
+      else if((digit == 'D') || (digit == 'E') || (digit == 'F')){
+        number = 3;
+      }
+      else if((digit == 'G') || (digit == 'H') || (digit == 'I')){
+        number = 4;
+      }
+      else if((digit == 'J') || (digit == 'K') || (digit == 'L')){
+        number = 5;
+      }
+      else if((digit == 'M') || (digit == 'N') || (digit == 'O')){
+        number = 6;
+      }
+      else if((digit == 'P') || (digit == 'Q') || (digit == 'R') || (digit == 'S')){
+        number = 7;
+      }
+      else if((digit == 'T') || (digit == 'U') || (digit == 'V') || (digit == 'W')){
+        number = 8;
+      }
+      else if((digit == 'X') || (digit == 'Y') || (digit == 'Z')){
+        number = 9;
+      }
+      else{
+        System.out.println("Please enter a valid number");
+      }
+    }while(number == 0)
 
     System.out.printf("The letter %s corresponds to the number %d", digit, number);
 
