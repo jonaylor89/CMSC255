a0dba631872d5a626d6e1f286e15856624195127
John Naylor <jonaylor89@gmail.com>
2017-10-05 12:50:46 -0400

    Comments

--- ProgrammingProjects/RocketData.java
+++ ProgrammingProjects/RocketData.java
@@ -15,28 +15,53 @@ import java.io.FileNotFoundException;
 
 public class RocketData{
 
+  // Making the force of gravity a global constant for every method
   private static final double GRAVITY = 0.009807;
 
   public static void main(String[] argv) throws FileNotFoundException{
 
+    /*********************************************************************************
+    *
+    * Grab the filename from the command line and convert it into a File object
+    * The File object will be scanned in with the Scanner object
+    *
+    *******************************************************************************/
     File rocketData = new File(argv[0]);
 
     Scanner fileReader = new Scanner(rocketData);
 
+    //Declaring needed variables
     String name;
     double thrust;
     double mass;
     double ratio;
     String typeOfRocket;
-
+    /*********************************************************************************
+    *
+    * Loop throught the scanned file until the file runs out of words
+    *
+    *******************************************************************************/
     while(fileReader.hasNext()){
 
+      /*********************************************************************************
+      *
+      * Values are whitespace delimited and are in the order 'String', 'Double', 'Double'.
+      * The scanner will extract the values in that order
+      *
+      *******************************************************************************/
       name = fileReader.next();
       mass = fileReader.nextDouble();
       thrust = fileReader.nextDouble();
 
+      // Calculation of the thrust-to-weight ratio
       ratio = thrustToWeight(thrust, mass);
 
+      /*********************************************************************************
+      *
+      * Check if the rocket is a rocket engine or a jet engine.
+      * If the thrust-to-weight is equal to twenty then then egine could be either
+      *
+      *******************************************************************************/
       if(ratio > 20.0){
         typeOfRocket = "Rocket Engine";
       }else if(ratio < 20.0){
@@ -45,11 +70,17 @@ public class RocketData{
         typeOfRocket = "Jet or Rocket Engine";
       }
 
+      // Output appropriate values in the specific format
      System.out.printf("%s %.4f %s\n", name, ratio, typeOfRocket);
 
     }
   }
 
+  /*************************************************************************************
+  *
+  * Helper method used to calculate the thrust-to-weight ratio of every rocket entry.
+  *
+  ************************************************************************************/
   public static double thrustToWeight(double thrust, double mass){
     return thrust / (mass * GRAVITY);
   }
